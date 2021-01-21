518ae3b115c2c78a80ea29abb54bf580851cb66b
abarnatan <ale.barn19@gmail.com>
2021-01-20 17:55:54 -0800

    filter for top 10 otu

--- static/app.js
+++ static/app.js
@@ -8,6 +8,22 @@ function getPlots(id) {
             console.log(sampleValues)
             var labels =  sampledata.samples[0].otu_labels.slice(0,10);
             console.log (labels) 
-            
+        // reduce to top 10 otu ids for the plot OTU and reversing it. 
+            var OTU_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+        // format the otu id's for the plot
+            var OTU_id = OTU_top.map(d => "OTU " + d);
+            console.log(`OTU IDS: ${OTU_id}`)
+         // get top 10 labels for the plot
+            var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+            console.log(`OTU_labels: ${labels}`)
+            var trace = {
+                x: sampleValues,
+                y: OTU_id,
+                text: labels,
+                marker: {
+                color: 'blue'},
+                type:"bar",
+                orientation: "h",
+            };  
         });
     }
