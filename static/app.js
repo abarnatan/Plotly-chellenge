ca0932da8f7cf83e1528b66a375b29e7dba53b6a
abarnatan <ale.barn19@gmail.com>
2021-01-20 18:02:55 -0800

    create bar chart

--- static/app.js
+++ static/app.js
@@ -25,5 +25,22 @@ function getPlots(id) {
                 type:"bar",
                 orientation: "h",
             };  
+         // create data variable
+            var data = [trace];
+        // create layout variable to set plots layout
+            var layout = {
+                title: "Top 10 OTU",
+                yaxis:{
+                    tickmode:"linear",
+                },
+                margin: {
+                    l: 100,
+                    r: 100,
+                    t: 100,
+                    b: 30
+                }
+        };
+     // create the bar plot
+        Plotly.newPlot("bar", data, layout);
         });
     }
